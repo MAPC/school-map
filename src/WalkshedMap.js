42ea4fdfa3e99d39ca1447581d7839294e9aee60
allthesignals <wilburnforce@gmail.com>
2017-06-18 13:41:34 -0400

    Accept properties; Add print button;
    This also coerces the incoming server geometries into a FeatureCollection to allow for feature-level properties.

--- src/WalkshedMap.js
+++ src/WalkshedMap.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Map, GeoJSON, Marker, TileLayer } from 'react-leaflet';
+import { Map, GeoJSON, CircleMarker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 import $ from 'jquery';
+import LeafletPrintPlugin from './LeafletPrintPlugin';
 import './WalkshedMap.css';
 
 const endpoint = window.endpoint || '/data/school2.json';
@@ -9,11 +10,14 @@ const bounds = [[40.712, -74.227],[40.774, -74.125]];
 const points = [ { coordinates: [42,-71]}, { coordinates: [42,-71]}];
 const shed_column_names = ['shed_05','shed_10','shed_15','shed_20'];
 const color_map = {
-    '2.0': "00FFFF",
-    '1.5': "#6600CC",
-    '1': "#9966FF",
-    '0.5': "#FF33FF"
+    'shed_20': "00FFFF",
+    'shed_15': "#6600CC",
+    'shed_10': "#9966FF",
+    'shed_05': "#FF33FF"
 };
+
+// initialize non-AMD/reacty leaflet plugin
+LeafletPrintPlugin();
 
 class WalkshedMap extends Component {
   constructor(props) {
@@ -27,16 +31,24 @@ class WalkshedMap extends Component {
   }
 
   componentDidMount() {
+    const map = this.refs.map.leafletElement;        
+    L.browserPrint().addTo(map);
     this.fetchSchoolData();
   }
 
   // filter for non-null values and roll them up into a Leaflet geoJSON object
   collect(cols, data) {
     return cols.filter((col) => {
-        return data[col]
+        return data[col];
       })
       .reduce((geojson, current) => {
-        return geojson.addData(data[current]);
+        let newObject = {};
+        if(data[current]) {
+          newObject.type = "Feature";
+          newObject.properties = { shed: current };
+          newObject.geometry = data[current];
+        }
+        return geojson.addData(newObject);
       }, L.geoJson(null));
   }
 
@@ -74,13 +86,27 @@ class WalkshedMap extends Component {
   render() {
     const geojson = () => {
         if(this.state.walksheds) {
-            return <GeoJSON data={this.state.walksheds} />
+            return <GeoJSON data={this.state.walksheds}
+                            style= { 
+                              (feature) => {
+                                console.log(feature);
+                                return {
+                                  color: color_map[feature.properties.shed],
+                                  weight: 1,
+                                  opacity: 0.3 
+                                } 
+                              }
+                            } />
+
         }
     }
 
     const school = () => {
       if(this.state.school) {
-        return <Marker position={this.state.school} />
+        return <CircleMarker  center={this.state.school}
+                              color={'red'}
+                              fillColor={'#f03'}
+                              fillOpacity={0.5} />
       }
     }
 
@@ -88,20 +114,27 @@ class WalkshedMap extends Component {
       let points = this.state.points;
       if (points.length) {
         return points.map((point, index) => 
-          <Marker position={[point.coordinates[1], point.coordinates[0]]} key={index} />
+          <CircleMarker center={[point.coordinates[1], point.coordinates[0]]} 
+                        key={index} 
+                        radius={3}
+                        fillColor={"#ff7800"}
+                        color={"#000"}
+                        weight={1}
+                        opacity={1}
+                        fillOpacity={0.8} />
         )
       }
     }
 
     return (
       <div className="WalkshedMap">
-        <Map bounds={this.state.bounds}>
+        <Map bounds={this.state.bounds} ref='map'>
           <TileLayer
             url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
           />
-          {survey_responses()}
           {geojson()}
           {school()}
+          {survey_responses()}
         </Map>
       </div>
     );
