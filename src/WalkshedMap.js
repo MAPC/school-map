17c5fe0c8f66a877e6b4aa7821409aee9b58c180
Eric Youngberg <eyoungberg@mapc.org>
2017-09-08 10:05:32 -0400

    Lift state to App component making WalkshedMap stateless

--- src/WalkshedMap.js
+++ src/WalkshedMap.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import { Map, GeoJSON, CircleMarker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
-import $ from 'jquery';
 import LeafletPrintPlugin from './LeafletPrintPlugin';
 import './WalkshedMap.css';
 
-const endpoint = window.endpoint || '/data/school.json';
-const bounds = [[40.712, -74.227],[40.774, -74.125]];
-
-
-const shedColumnNames = ['shed_05','shed_10','shed_15','shed_20'];
 const shedColorMap = {
     'shed_20': "00FFFF",
     'shed_15': "#6600CC",
     'shed_10': "#9966FF",
     'shed_05': "#FF33FF"
 };
-
 
 const modes = {
   'w':  { color: '#004DF1', name: 'Walk' },
@@ -33,51 +26,10 @@ const modes = {
 LeafletPrintPlugin();
 
 class WalkshedMap extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      bounds,
-      points: [],
-      walksheds: false
-    }
-  }
 
   componentDidMount() {
     const map = this.refs.map.leafletElement;        
     L.browserPrint().addTo(map);
-    this.fetchSchoolData();
-  }
-
-  // filter for non-null values and roll them up into a Leaflet geoJSON object
-  collect(cols, data) {
-    return cols.filter((col) => {
-        return data[col];
-      })
-      .reduce((geojson, current) => {
-        let newObject = {};
-        if(data[current]) {
-          newObject.type = "Feature";
-          newObject.properties = { shed: current };
-          newObject.geometry = data[current];
-        }
-        return geojson.addData(newObject);
-      }, L.geoJson(null));
-  }
-
-  fetchSchoolData() {
-    return $.getJSON(endpoint)
-      .then((data) => {
-
-        // flatten survey responses across surveys into one
-        const responses = data.surveys.reduce((a,b) => a.concat(b.survey_responses), []);
-        const sheds = this.collect(shedColumnNames, data);
-
-        this.setState({ bounds: sheds.getBounds(),
-                        points: responses,
-                        walksheds: sheds.toGeoJSON(),
-                        school: { lat: data.wgs84_lat, lng: data.wgs84_lng } });
-      });
   }
 
   style = (feature) => {
@@ -88,8 +40,8 @@ class WalkshedMap extends Component {
 
   render() {
     const geojson = () => {
-        if(this.state.walksheds) {
-            return <GeoJSON data={this.state.walksheds}
+        if (this.props.walksheds) {
+            return <GeoJSON data={this.props.walksheds}
                             style= { 
                               (feature) => {
                                 return {
@@ -101,19 +53,20 @@ class WalkshedMap extends Component {
                             } />
 
         }
-    }
+    };
 
     const school = () => {
-      if(this.state.school) {
-        return <CircleMarker  center={this.state.school}
+      if(this.props.school) {
+        return <CircleMarker  center={this.props.school}
                               color={'red'}
                               fillColor={'#f03'}
                               fillOpacity={0.5} />
       }
-    }
+    };
 
     const survey_responses = () => {
-      let points = this.state.points;
+      const points = this.props.points;
+
       if (points.length > 0) {
         return points.map((point, index) => {
           var geom = point.geometry;
@@ -128,11 +81,11 @@ class WalkshedMap extends Component {
                         fillOpacity={0.8} />
         });
       }
-    }
+    };
 
     return (
       <div className="WalkshedMap">
-        <Map bounds={this.state.bounds} ref='map'>
+        <Map bounds={this.props.bounds} ref='map'>
           <TileLayer
             url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
           />
