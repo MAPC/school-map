0d0bd32a80cdb6e52b6f398ca79125f92f263397
Eric Youngberg <eyoungberg@mapc.org>
2019-01-28 13:41:38 -0500

    Use random port

--- render.js
+++ render.js
@@ -4,37 +4,38 @@
 
 'use strict';
 
-const os          = require('os');
 const http        = require('http');
+const path        = require('path');
 const final       = require('finalhandler');
 const puppeteer   = require('puppeteer');
 const serveStatic = require('serve-static');
 
+const { argv, exit } = process;
+
 
 async function main() {
-  if (process.argv.length < 3) {
-    os.exit(1);
+  if (argv.length < 3) {
+    exit(1);
   }
 
-  const schoolId = process.argv[2];
-  const port = 3003;
+  const schoolId = argv[2];
 
   let server = null;
   try {
-    server = startServer(port);
+    server = startServer();
   }
   catch(e) {
-    os.exit(1);
+    exit(1);
   }
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport(calcDims(80));
+  await page.setViewport(calcDims(100));
   await page.evaluateOnNewDocument(schid => {
     window.endpoint = `/data/${schid}.json`;
     window.staticRender = true;
   }, schoolId);
-  await page.goto(`http://localhost:${port}`, { waitUntil: 'networkidle0' });
+  await page.goto(`http://localhost:${server.address().port}`, { waitUntil: 'networkidle0' });
   await page.screenshot({ path: path.join('screens', `${schoolId}.png`) });
   await browser.close();
 
@@ -43,20 +44,20 @@ async function main() {
 }; main();
 
 
-function startServer(port) {
+function startServer() {
   const serveFile = serveStatic('build', {index: ['index.html']});
 
   return (
     http
       .createServer((req, res) => serveFile(req, res, final(req, res)))
-      .listen(port)
+      .listen(0) // listen 0 for random port
   );
 }
 
 
 function calcDims(scale) {
   return {
-    width: 9 * scale,
+    width: 10 * scale,
     height: 6 * scale,
   };
 }
